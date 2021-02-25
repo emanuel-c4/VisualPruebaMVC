07db6c5cef11226eeff0f3923e9017113bc4f72b
Emanuel Ciotoi <emanuel12m@gmail.com>
2021-02-26 00:11:30 +0100

    first commit

---
+++ PruebaMVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult prueba(string name ="" , int numTimes = 1)
+        {
+            ViewData["Nombre"] = name;
+            ViewData["NumVeces"] = numTimes;
+            ViewBag.Message = "Hola " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
